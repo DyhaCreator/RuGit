8d8f60472a17782bb94630bd0d9d142d1a8c268f
DyhaCreator <91116965+DyhaCreator@users.noreply.github.com>
2022-05-20 23:22:28 +0500

    Add files via upload

---
+++ RuGit/RuGit.js
@@ -0,0 +1,8 @@
+var git = document.getElementById('1')
+var but = document.getElementById('rep')
+git.innerHTML='RuGit'
+git.style.color='red'
+git.style.textAlign = 'left'
+git.style.backgroundColor = 'black'
+git.style.padding = '1rem'
+console.dir(git)
